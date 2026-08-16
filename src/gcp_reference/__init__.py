14a5bb37ee1696f44c813d6138d6a044d3e261b4
Vignesh412 <iyer.vignesh2@gmail.com>
2026-08-16 08:49:58 +0530

    Implement stateful enforcement slice: update src/gcp_reference/__init__.py

--- src/gcp_reference/__init__.py
+++ src/gcp_reference/__init__.py
@@ -1,15 +1,25 @@
 """Governance Capsule Protocol candidate reference library."""
 
+from .allocation import AllocationLedger
 from .crypto import KeyResolver, artifact_digest, sign_artifact, verify_artifact
 from .errors import ErrorCode, GCPError
-from .semantics import validate_delegation, validate_delegation_proof
+from .replay import UseRegistry
+from .revocation import RevocationEvaluator, RevocationEvidence, RevocationResult, StatusRecord
+from .semantics import validate_audience, validate_delegation, validate_delegation_proof
 
 __all__ = [
+    "AllocationLedger",
     "ErrorCode",
     "GCPError",
     "KeyResolver",
+    "RevocationEvaluator",
+    "RevocationEvidence",
+    "RevocationResult",
+    "StatusRecord",
+    "UseRegistry",
     "artifact_digest",
     "sign_artifact",
+    "validate_audience",
     "validate_delegation",
     "validate_delegation_proof",
     "verify_artifact",
