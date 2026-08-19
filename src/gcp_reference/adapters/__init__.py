1fd022e619757a6d56ad2a3595b3a4966f939dc6
Vignesh412 <iyer.vignesh2@gmail.com>
2026-08-19 12:11:46 +0530

    Update src/gcp_reference/adapters/__init__.py for cross-framework guided demo

--- src/gcp_reference/adapters/__init__.py
+++ src/gcp_reference/adapters/__init__.py
@@ -1,5 +1,23 @@
 """Optional policy-runtime adapters."""
 
 from .microsoft_acs import ACSInterventionRuntime
+from .openai_agents import (
+    OpenAIGovernanceHandoffAdapter,
+    OpenAIHandoffState,
+    build_openai_on_handoff,
+)
+from .google_adk import (
+    AcceptedADKDelegation,
+    GoogleADKGovernanceBoundary,
+    build_google_adk_before_tool_callback,
+)
 
-__all__ = ["ACSInterventionRuntime"]
+__all__ = [
+    "ACSInterventionRuntime",
+    "AcceptedADKDelegation",
+    "GoogleADKGovernanceBoundary",
+    "OpenAIGovernanceHandoffAdapter",
+    "OpenAIHandoffState",
+    "build_google_adk_before_tool_callback",
+    "build_openai_on_handoff",
+]
